47b1de108ac21bd80eaf96bfa28cc7ff7a12a190
Laurent Marquet <laurent.marquet@laposte.net>
2018-10-17 14:01:26 +0200

    Ajout des champs "createdAt", "updatedAt", "createdBy" et "updatedBy" (17/10/2018)

--- src/Entity/Child.php
+++ src/Entity/Child.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use App\Entity\Traits\CreationTrait;
 
 /**
  * Child
@@ -14,6 +15,8 @@ use Doctrine\Common\Collections\Collection;
  */
 class Child
 {
+    use CreationTrait;
+
     /**
      * @var int
      *
---
+++ src/Entity/Traits/CreationTrait.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Entity\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+
+trait CreationTrait
+{
+    /**
+     * @var datetime
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="created_by", type="integer")
+     */
+    private $createdBy;
+
+    /**
+     * @var datetime
+     *
+     * @ORM\Column(name="updated_at", type="datetime")
+     */
+    private $updatedAt;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="updated_by", type="integer")
+     */
+    private $updatedBy;
+
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(?\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
