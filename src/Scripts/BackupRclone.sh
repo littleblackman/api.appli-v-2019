e726b1689c3ccfe2cff4088141a4c47f3ef7256b
Laurent Marquet <laurent.marquet@laposte.net>
2019-02-05 09:28:21 +0100

    v0.25.2 - Ajout du nouveau site à la sauvegarde (05/02/2019)

--- src/Scripts/BackupRclone.sh
+++ src/Scripts/BackupRclone.sh
@@ -6,33 +6,34 @@
 #
 # Script to backup the specified folders via rclone to external computer (SwissBackup - 975L.com)
 
-Folder="/var/www/vhosts/appli-v.net";
+Hosts="/var/www/vhosts";
 Rclone="sb_project_SBI-LM966748:default";
 
 #Synchronizes the appli-v.net folder
-rclone sync $Folder/ $Rclone/appli-v.net --skip-links --exclude cache/ --exclude backup/ --exclude dev/ --delete-excluded;
+rclone sync $Hosts/appli-v.net $Rclone/appli-v.net --skip-links --exclude cache/ --exclude backup/ --exclude dev/ --delete-excluded;
+
+#Synchronizes the energykidsacademy.net folder
+rclone sync $Hosts/energykidsacademy.net $Rclone/energykidsacademy.net --skip-links --exclude cache/ --exclude backup/ --exclude dev/ --delete-excluded;
 
 #Copies the backup files (original are still on the server)
-rclone copy $Folder/api.appli-v.net/var/backup $Rclone/backup;
+rclone copy $Hosts/appli-v.net/api.appli-v.net/var/backup $Rclone/backup;
 
 ######################################### Restore ###############################################
 # The computer has to have a copy of the config file located in /root/.config/rclone/rclone.conf
 #
 # Full instructions available at https://rclone.org/commands/rclone/
 #
-# Check $Rclone value above
-#
 # How to restore appli-v.net folder
-# rclone sync $Rclone/appli-v.net /path/for/restore/ --progress;
+# rclone sync sb_project_SBI-LM966748:default/appli-v.net /path/for/restore/ --progress;
 #
 # How to list the content of the backup
-# rclone ls $Rclone/appli-v.net;
+# rclone ls sb_project_SBI-LM966748:default/appli-v.net;
 #
 # How to list only the directories of the content of the backup
-# rclone lsd $Rclone/backup;
+# rclone lsd sb_project_SBI-LM966748:default/backup;
 #
 # How to purge folders
-# rclone purge $Rclone/backup/2019/2019-01;
+# rclone purge sb_project_SBI-LM966748:default/backup/2019/2019-01;
 #################################################################################################
 
 exit 0
