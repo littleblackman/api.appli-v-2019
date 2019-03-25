bcf91e25eb2379c741079d23450de9ec49e786fd
littleblackman <sandyrazafitrimo@gmail.com>
2019-03-25 23:51:43 +0100

    add join table taskStaff

---
+++ src/Entity/TaskStaff.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TaskStaffRepository")
+ */
+class TaskStaff
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var Staff
+     *
+     * @ORM\OneToOne(targetEntity="Staff")
+     * @ORM\JoinColumn(name="staff_id", referencedColumnName="staff_id")
+     */
+    private $staff;
+
+    /**
+     * @var Task
+     *
+     * @ORM\OneToOne(targetEntity="Task")
+     * @ORM\JoinColumn(name="task_id", referencedColumnName="task_id")
+     */
+    private $staff;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateTask;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateTask(): ?\DateTimeInterface
+    {
+        return $this->dateTask;
+    }
+
+    public function setDateTask(\DateTimeInterface $dateTask): self
+    {
+        $this->dateTask = $dateTask;
+
+        return $this;
+    }
+
+    public function getStaff(): ?Staff
+    {
+        return $this->staff;
+    }
+
+    public function setStaff(?Staff $staff): self
+    {
+        $this->staff = $staff;
+
+        return $this;
+    }
+
+    public function getTask(): ?Task
+    {
+        return $this->task;
+    }
+
+    public function setTask(?Task $task): self
+    {
+        $this->task = $task;
+
+        return $this;
+    }
+}
